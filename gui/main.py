3702b759275e511891526e5ce41bca3dcbb016ce
gandres2416 <gandres2416@gmail.com>
2024-03-07 09:59:09 -0600

    update map

--- gui/main.py
+++ gui/main.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from networktables import NetworkTables
+import os
+import time
 
 NetworkTables.initialize(server='100.71.254.107')
 nt = NetworkTables.getTable("localization")
@@ -10,10 +12,12 @@ from pygame.locals import (
     K_q
 )
 
-BLOCK_SIZE = 8
+BLOCK_SIZE = 5
 
-GRID_M_WIDTH = 4
+GRID_M_WIDTH = 5
 GRID_M_HEIGHT = 3
+
+GRID_Y_OFFSET = 0.08
 
 BLOCKS_PER_METER = 50
 GRID_WIDTH = (GRID_M_WIDTH * BLOCKS_PER_METER) + 1
@@ -41,10 +45,16 @@ def draw_pixel(x, y, color):
     rect = pygame.Rect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
     pygame.draw.rect(screen, color, rect)
 
+def draw_fused(x, y):
+    draw_pixel(x * BLOCKS_PER_METER, y * BLOCKS_PER_METER, GREEN)
+
+def draw_dwm(x, y):
+    draw_pixel(x * BLOCKS_PER_METER, y * BLOCKS_PER_METER, RED)
+
 def draw_grid():
     for x in range(GRID_WIDTH):
         for y in range(GRID_HEIGHT):
-            if x % BLOCKS_PER_METER == 0 or y % BLOCKS_PER_METER == 0:
+            if x % int(BLOCKS_PER_METER * .61) == 0 or (y - int(BLOCKS_PER_METER * GRID_Y_OFFSET)) % int(BLOCKS_PER_METER * .61) == 0:
                 draw_pixel(x, y, GREY)
 
 def main():
@@ -61,7 +71,14 @@ def main():
                     sys.exit(0)
         screen.fill(BLACK)
         draw_grid()
-        draw_pixel(nt.getNumber('x', -1), nt.getNumber('y', -1), RED)
+        draw_fused(nt.getNumber('dwm_x', -1), -1 * nt.getNumber('dwm_y', -1))
+        draw_dwm(nt.getNumber('fuse_x', -1), -1 * nt.getNumber('fuse_y', -1))
         pygame.display.update()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(round(nt.getNumber('fuse_x', -1), 3))
+        print(round(nt.getNumber('fuse_y', -1), 3))
+        print(nt.getNumber('dwm_x', -1))
+        print(nt.getNumber('dwm_y', -1))
+        time.sleep(1/100)
 
 main()

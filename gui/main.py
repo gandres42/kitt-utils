02dacd414f0122adc9f8fa2a7ae3e2e8ac9797ff
gandres42 <gandres2416@gmail.com>
2024-05-12 14:38:34 -0400

    abusing for csm path planning

--- gui/main.py
+++ gui/main.py
@@ -3,6 +3,7 @@ import pygame
 from networktables import NetworkTables
 import os
 import time
+import math
 
 NetworkTables.initialize(server='192.168.1.109')
 nt = NetworkTables.getTable("localization")
@@ -14,12 +15,11 @@ from pygame.locals import (
 
 BLOCK_SIZE = 5
 
-GRID_M_WIDTH = 3
+GRID_M_WIDTH = 5
 GRID_M_HEIGHT = 3
 
-GRID_Y_OFFSET = 0.08
-# GRID_Y_OFFSET = 0
-GRID_MEASURE = .61
+GRID_Y_OFFSET = 0
+GRID_MEASURE = 1
 
 BLOCKS_PER_METER = 50
 GRID_WIDTH = (GRID_M_WIDTH * BLOCKS_PER_METER) + 1
@@ -47,14 +47,14 @@ def draw_pixel(x, y, color):
     rect = pygame.Rect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
     pygame.draw.rect(screen, color, rect)
 
-def draw_fuse(x, y):
-    draw_pixel(x * BLOCKS_PER_METER, y * BLOCKS_PER_METER, GREEN)
+def draw_path(x, y):
+    draw_pixel(x, y, GREEN)
 
-def draw_dwm(x, y):
-    draw_pixel(x * BLOCKS_PER_METER, y * BLOCKS_PER_METER, RED)
+def draw_pos(x, y):
+    draw_pixel(x, y, RED)
 
-def draw_vive(x, y):
-    draw_pixel(x * BLOCKS_PER_METER, y * BLOCKS_PER_METER, BLUE)
+def draw_carrot(x, y):
+    draw_pixel(x, y, BLUE)
 
 def draw_grid():
     for x in range(GRID_WIDTH):
@@ -62,12 +62,29 @@ def draw_grid():
             if x % int(BLOCKS_PER_METER * GRID_MEASURE) == 0 or (y - int(BLOCKS_PER_METER * GRID_Y_OFFSET)) % int(BLOCKS_PER_METER * GRID_MEASURE) == 0:
                 draw_pixel(x, y, GREY)
 
+def motor_velocities(theta, magnitude):
+    joy_x = math.sin(theta) * magnitude
+    joy_y = math.cos(theta) * magnitude
+    if abs(joy_x) > abs(joy_y):
+        max_r = abs(magnitude / joy_x)
+    else:
+        max_r = abs(magnitude / joy_y)
+
+    # this is the actual throttle  
+    magnitude = magnitude / max_r
+    turn_damping = 3.0 # for example
+    right = (math.sin(theta) + math.cos(theta) / turn_damping)
+    left = (math.sin(theta) - math.cos(theta) / turn_damping)
+    
+    print(tuple([left, right]))
+    
+
 def main():
     running = True
 
-    vive = []
-    dwm = []
-    fuse = []
+    path = [(i, int(math.sin(i*.05) * 50) + 75) for i in range(0, GRID_WIDTH)]
+    i = 0
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -76,21 +93,45 @@ def main():
             elif event.type == KEYDOWN:
                 if event.key == K_q:
                     pygame.quit()
-                    print('\nvive')
-                    print(vive)
-                    print('\nfuse')
-                    print(fuse)
-                    print('\ndwm')
-                    print(dwm)
                     sys.exit(0)
         draw_grid()
-        draw_fuse(nt.getNumber('fuse_x', -1000) + 3.8, (-1 * nt.getNumber('fuse_y', -1000)) + .8)
-        draw_dwm(nt.getNumber('dwm_x', -1000) + 3.8, (-1 * nt.getNumber('dwm_y', -1000)) + .8)
-        draw_vive(nt.getNumber('vive_x', -1000) + 3.8, (-1 * nt.getNumber('vive_y', -1000)) + .8)
+        for point in path:
+            draw_path(point[0], point[1])
 
-        fuse.append((nt.getNumber('fuse_x', -1000) + 3.8, (-1 * nt.getNumber('fuse_y', -1000)) + .8))
-        dwm.append((nt.getNumber('dwm_x', -1000) + 3.8, (-1 * nt.getNumber('dwm_y', -1000)) + .8))
-        vive.append((nt.getNumber('vive_x', -1000) + 3.8, (-1 * nt.getNumber('vive_y', -1000)) + .8))
+        draw_pos(path[i][0], path[i][1])
+        
+        # todo: find first element from end of path within carrot distance
+        p1 = (path[i][0], path[i][1])
+        dst = None
+        p2 = None
+        # j = i + min(len(path) - i - 1, 5)
+        for j in reversed(range(len(path))):
+            p2 = (path[j][0], path[j][1])
+            dst = math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+            if dst <= 20:
+                break
+        draw_carrot(p2[0], p2[1])
+        try:
+            theta = abs(math.degrees(math.atan((p1[1] - p2[1])/(p2[0] - p1[0]))))
+        except ZeroDivisionError:
+            theta = 0
+
+        if p2[0] > p1[0] and p2[1] > p1[1]:
+            theta = 90 + theta
+        if p2[0] > p1[0] and p2[1] < p1[1]:
+            theta = 90 - theta
+        if p2[0] < p1[0] and p2[1] > p1[1]:
+            theta = -90 + theta
+        if p2[0] < p1[0] and p2[1] < p1[1]:
+            theta = -90 - theta
+        
+        motor_velocities(math.radians(theta), dst)
+        
+
+        i = i + 1
+        if i >= GRID_WIDTH:
+            i = 0
         pygame.display.update()
+        time.sleep(.05)
 
 main()
